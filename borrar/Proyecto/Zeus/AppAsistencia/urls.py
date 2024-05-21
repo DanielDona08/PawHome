e352110a2d338643657c79e25dd3533e13fdf19c
Daniel Donato <danieldona1415@gmail.com>
2024-05-20 23:52:01 -0500

    prueba 1

---
+++ borrar/Proyecto/Zeus/AppAsistencia/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path 
+from . import views
+
+from django.conf import settings
+from django.contrib.staticfiles.urls import static
+
+urlpatterns = [
+    path('', views.inicio, name ='inicio'),
+    path('nosotros/', views.nosotros, name='nosotros'),
+    path('estudiantes/', views.estudiantes, name='estudiantes'),
+    path('estudiantes/crear', views.crear, name='crear'),
+    path('estudiantes/editar', views.editar, name='editar'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
