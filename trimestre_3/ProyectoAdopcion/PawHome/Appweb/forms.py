546d847c8efd40f734e24fa82ac7a0eb34e35ea6
Aleee00 <aleeejo68@gmail.com>
2024-06-05 15:36:38 -0500

    actualizacion templates

--- trimestre_3/ProyectoAdopcion/PawHome/Appweb/forms.py
+++ trimestre_3/ProyectoAdopcion/PawHome/Appweb/forms.py
@@ -1,25 +1,16 @@
 from django import forms
-from .models import Mascota
+from .models import Raza
 
 class BusquedaAvanzadaForm(forms.Form):
-    TIPO_MASCOTA_CHOICES = [
-        ('perro', 'Perro'),
-        ('gato', 'Gato'),
-    ]
+    tipoMascota = forms.ChoiceField(choices=Raza.TIPO_CHOICES, required=True)
+    razaMascota = forms.ModelChoiceField(queryset=Raza.objects.none(), required=True)
+    generoMascota = forms.ChoiceField(choices=[('macho', 'Macho'), ('hembra', 'Hembra')], required=True)
+    colorMascota = forms.ChoiceField(choices=[('blanco', 'Blanco'), ('negro', 'Negro'), ('marron', 'Marrón'), ('gris', 'Gris'), ('dorado', 'Dorado'), ('atigrado', 'Atigrado')], required=True)
+    tamanoMascota = forms.ChoiceField(choices=[('pequeno', 'Pequeño'), ('mediano', 'Mediano'), ('grande', 'Grande')], required=True)
 
-    GENERO_CHOICES = [
-        ('macho', 'Macho'),
-        ('hembra', 'Hembra'),
-    ]
+    def __init__(self, *args, **kwargs):
+        tipoMascota = kwargs.pop('tipoMascota', None)
+        super(BusquedaAvanzadaForm, self).__init__(*args, **kwargs)
+        if tipoMascota:
+            self.fields['razaMascota'].queryset = Raza.objects.filter(tipo=tipoMascota)
 
-    TAMANO_CHOICES = [
-        ('pequeno', 'Pequeño'),
-        ('mediano', 'Mediano'),
-        ('grande', 'Grande'),
-    ]
-
-    tipo = forms.ChoiceField(choices=TIPO_MASCOTA_CHOICES, required=True)
-    raza = forms.CharField(max_length=50, required=True)
-    genero = forms.ChoiceField(choices=GENERO_CHOICES, required=True)
-    color = forms.CharField(max_length=20, required=True)
-    tamano = forms.ChoiceField(choices=TAMANO_CHOICES, required=True)
--- trimestre_3/ProyectoAdopcion/PawHome/PawHome/settings.py
+++ trimestre_3/ProyectoAdopcion/PawHome/PawHome/settings.py
@@ -10,10 +10,28 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],  
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 
 
 # Quick-start development settings - unsuitable for production
