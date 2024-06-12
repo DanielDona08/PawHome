c6de55fa70b5ed993b3546ee064efdfa65889c94
DanielDona08 <danieldona1415@gmail.com>
2024-06-12 17:11:17 -0500

    Actualizacion Formulario Publicacion

--- trimestre_3/ProyectoAdopcion/PawHome/Appweb/urls.py
+++ trimestre_3/ProyectoAdopcion/PawHome/Appweb/urls.py
@@ -1,13 +1,10 @@
-from django.urls import path
+from django.urls import include, path
 from . import views
 
 urlpatterns = [
-  
-  path('', views.inicio, name='inicio'),
-  path('adopcion/', views.adopcion, name='adopcion'),
-  path('publicacion/', views.publicacion, name='publicacion'),
-  path('busqueda_avanzada/', views.busqueda_avanzada, name='busqueda_avanzada'),
-  
-
+    path('', views.inicio, name='inicio'),
+    path('adopcion/', views.adopcion, name='adopcion'),
+    path('publicacion/', views.publicacion, name='publicacion'),
+    path('busqueda_avanzada/', views.busqueda_avanzada, name='busqueda_avanzada'),
+    path('obtener-razas-mascota/', views.obtener_razas_mascota, name='obtener_razas_mascota'),
 ]
-	
