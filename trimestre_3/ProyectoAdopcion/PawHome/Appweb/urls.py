3d3abfb51b820082c68d7d26533f030e86b45adb
Daniel Donato <danieldona1415@gmail.com>
2024-06-18 16:12:52 -0500

    Mis Favoritos (Actualización)

--- trimestre_3/ProyectoAdopcion/PawHome/Appweb/urls.py
+++ trimestre_3/ProyectoAdopcion/PawHome/Appweb/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('busqueda_avanzada/', views.busqueda_avanzada, name='busqueda_avanzada'),
     path('obtener-razas-mascota/', views.obtener_razas_mascota, name='obtener_razas_mascota'),
     path('mascota/<int:pk>/', views.detalle_mascota, name='detalle_mascota'),
+    path('mascota/<int:pk>/favorito/', views.detalle_mascota, name='agregar_favorito'),
+    path('favoritos/', views.favoritos, name='favoritos'),
 ]
